bcc4b52c3b7cf38e46f0f7690a6602f7fc2f20e8
angnkv22 <angel.neikov@gmail.com>
2020-11-02 19:06:45 +0000

    .

--- src/exercise5/UniqueNumbers.java
+++ src/exercise5/UniqueNumbers.java
@@ -1,9 +1,8 @@
 package exercise5;
+  import java.util.Scanner;
+  import java.util.Arrays;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class UniqueNumbers {
+  public class UniqueNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
@@ -18,7 +17,20 @@ public class UniqueNumbers {
         }
 
         int[] uniqueNumbers = new int[numbers.length];
-        // Find unique numbers in numbers
+
+	      for(int i=0; i<numbers.length; i++){
+
+          for(int j=0; j<uniqueNumbers.length; j++){
+            if(uniqueNumbers[j]==0){
+              uniqueNumbers[j]= numbers[i];
+              break;
+          
+            }
+            else if (uniqueNumbers[j]==numbers[i]){
+              break;
+            }
+          }
+        }
 
         String uniqueNumbersAsString = Arrays.toString(uniqueNumbers);
         System.out.println("Unique numbers: " + uniqueNumbersAsString);
